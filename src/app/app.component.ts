4b18aabaf1b756cf828f8f2ed24874d1176aa965
Caroline <wanguic49@gmail.com>
2024-07-04 17:00:30 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'clients-dashboard';
+
+  data:any[] =[];
+
+  constructor(private dataService:DataService){}
+
+
+  ngOnInit(): void {
+      this.dataService.getData().subscribe((data=>{
+          this.data = data;
+      }),
+      (err)=>{
+        console.log(err);
+        
+      }
+    );
+  }
 }
